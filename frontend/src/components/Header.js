c69c2aa402b440c869cf011ae056e29fb1563086
Jayrhald Padilla <jayrhald.jp@gmail.com>
2022-11-23 17:16:01 +0800

    add logo

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import {Navbar, Container} from 'react-bootstrap';
+import { ReactComponent as Logo } from '../images/logo.svg'
 
 const navbarStyle = {
-  backgroundColor: 'lightblue'
+  backgroundColor: '#eeee'
 }
 
 const Header = (props) => {
     return(
         <Navbar style ={navbarStyle}  variant="light">
           <Container>
-            <Navbar.Brand href="/">{props.title}</Navbar.Brand>
+            <Logo style = {{maxWidth: '12rem', maxHeight: '2rem'}}></Logo>
           </Container>
         </Navbar>
     );
