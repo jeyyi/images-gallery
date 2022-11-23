944c939f132f3030dfb4ea014d61196895a9fce1
Jayrhald Padilla <jayrhald.jp@gmail.com>
2022-11-23 16:00:07 +0800

    image card added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,40 @@
 //import logo from './logo.svg';
-import { useState } from 'react';
-import Header from './components/Header';
-import Search from './components/Search';
-import 'bootstrap/dist/css/bootstrap.min.css';
- 
+import { useState } from "react";
+import Header from "./components/Header";
+import Search from "./components/Search";
+import "bootstrap/dist/css/bootstrap.min.css";
+import ImageCard from "./components/ImageCard";
+import { Container, Row, Col } from "react-bootstrap";
+
 const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
-
-const App = () => {
-  const [word, setWord] = useState(''); 
-
-  const handleSearchSubmit = (e) =>{
+function App() {
+  const [word, setWord] = useState("");
+  const [images, setImages] = useState([]);
+  const handleSearchSubmit = (e) => {
     e.preventDefault();
     console.log(word);
-    fetch(`https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`)
-    .then((res) => res.json())
-    .then((data) => {
-      console.log(data)
-
-    }) 
-    .catch((err)=>{
-      console.log(err);
-    })
-    setWord('')
-  }
-
+    fetch(
+      `https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setImages([data, ...images])
+        console.log(images);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    setWord("");
+  };
   return (
     <div>
-        <Header title = "Images Gallery"></Header>
-        <Search word = {word} setWord = {setWord} 
-        handleSubmit = {handleSearchSubmit}></Search>
+      <Header title="Images Gallery"></Header>
+      <Search
+        word={word}
+        setWord={setWord}
+        handleSubmit={handleSearchSubmit}
+      ></Search>
+      
     </div>
   );
 }
