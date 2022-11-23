50e323ddb08d7f1f7c2615debd2472137d1a9c52
Jayrhald Padilla <jayrhald.jp@gmail.com>
2022-11-23 13:21:15 +0800

    clear input

--- frontend/src/components/Search.js
+++ frontend/src/components/Search.js
@@ -4,15 +4,19 @@ import Form from "react-bootstrap/Form";
 import Row from "react-bootstrap/Row";
 import {Container} from "react-bootstrap"
 
-const Search = () => {
+const Search = ({word, setWord, handleSubmit}) => {
   return (
     <Container className="mt-4">
         <Row className="justify-content-center">
             <Col xs={12} md={8}>
-                <Form>
+                <Form onSubmit={handleSubmit}>
                     <Form.Row>
                         <Col xs={9}>
-                            <Form.Control placeholder="Search for new image.."/>
+                            <Form.Control 
+                            type = "text"
+                            value = {word}
+                            onChange = {(e) => setWord(e.target.value)}
+                            placeholder="Search for new image.."/>
                         </Col>
                         <Col>
                             <Button variant="primary" type = "submit">Search</Button>
